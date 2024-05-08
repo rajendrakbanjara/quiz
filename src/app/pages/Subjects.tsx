4caf6b5451ecd93d89b92c7a6751223d38115b5b
Rajendra Kumar <rajendrakbanjara@gmail.com>
2024-05-09 01:01:36 +0530

    nominal changes updated for Desktop view

--- src/app/pages/Subjects.tsx
+++ src/app/pages/Subjects.tsx
@@ -30,25 +30,25 @@ const Subjects: React.FC<ChildProps> = ({chooseSubject})=>{
           <div>
             <div>
               <ul className="space-y-4">
-                <li onClick={()=> chooseSubject(1)}className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-2 w-[36rem] rounded-2xl text-2xl">
+                <li onClick={()=> chooseSubject(1)}className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-1.5 w-[36rem] rounded-2xl text-2xl hover:border-purple-600 hover:border-4 border-4 border-transparent hover:transition border-opacity-0 hover:border-opacity-100 hover:duration-700">
                   <div className="bg-orange-100 p-4 ml-2 my-2 rounded-md  mr-7 text-2xl">
                     <FaCode color="orange" size="25px" />
                   </div>
                    HTML
                 </li>
-                <li onClick={()=>chooseSubject(2) }className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-2 w-[36rem] rounded-2xl text-2xl">
+                <li onClick={()=>chooseSubject(2) }className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-1.5 w-[36rem] rounded-2xl text-2xl hover:border-purple-600 hover:border-4 border-4 border-transparent hover:transition border-opacity-0 hover:border-opacity-100 hover:duration-700">
                   <div className="bg-green-100 p-4 ml-2 my-2 rounded-md  mr-7 text-2xl">
                     <LuPaintbrush2 color="Green" size="25px" />
                   </div>{" "}
                   CSS
                 </li>
-                <li onClick={()=> chooseSubject(3)} className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-2 w-[36rem] rounded-2xl text-2xl">
+                <li onClick={()=> chooseSubject(3)} className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-1.5 w-[36rem] rounded-2xl text-2xl hover:border-purple-600 hover:border-4 border-4 border-transparent hover:transition border-opacity-0 hover:border-opacity-100 hover:duration-700">
                   <div className="bg-blue-100 p-4 ml-2 my-2 rounded-md  mr-7 text-2xl">
                     <RiJavascriptLine color="Blue" size="25px" />
                   </div>
                   JavaScritp
                 </li>
-                <li onClick={()=> chooseSubject(4)} className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-2 w-[36rem] rounded-2xl text-2xl">
+                <li onClick={()=> chooseSubject(4)} className="flex items-center font-bold text-slate-600 dark:text-white bg-slate-100 dark:bg-slate-600 p-1.5 w-[36rem] rounded-2xl text-2xl hover:border-purple-600 hover:border-4 border-4 border-transparent hover:transition border-opacity-0 hover:border-opacity-100 hover:duration-700">
                   <div className="bg-purple-100 p-4 ml-2 my-2 rounded-md  mr-7 text-2xl">
                     <IoAccessibilityOutline size="25px" color="purple" />
                   </div>
